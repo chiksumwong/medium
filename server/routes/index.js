75606460c97978a7e6a018b0669a42e7ccdf8add
CHIK SUM <chiksum.wong@gmail.com>
2018-09-08 17:27:24 +0800

    fix

--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-// Import route page
-const user = require('./user')
-const article = require('./article')
-
-module.exports = (router) => {
-    // Put 'router' to route page
-    user(router)
-    article(router)
-}
